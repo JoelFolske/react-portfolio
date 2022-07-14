bb9c3683f923b2d80444699c89c0dc96f9078827
Joelfolske <joel_folske@icloud.com>
2022-07-14 14:13:04 -0500

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -136,7 +136,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="linkedin.com/in/joel-f-291883161"
+              href="joelfolske00@gmail.com"
             >
               Email <HiOutlineMail size={30} />
             </a>
@@ -145,7 +145,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="linkedin.com/in/joel-f-291883161"
+              href="https://github.com/JoelFolske"
             >
               Github <FaGithub size={30} />
             </a>
